0f58da867d6231d4420a0cab5c6f1c8ea043cac6
Muzahidul Islam <95979968+MuzahidulIslam1@users.noreply.github.com>
2025-01-21 16:32:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,5 @@ def predictRoute():
 
 if __name__ == "__main__":
     clApp = ClientApp()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
 
